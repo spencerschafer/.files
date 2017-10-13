b38edae3442fc7393a4815a984506db27ab497ae
spencerschafer <spencerschafer@gmail.com>
2017-10-13 14:31:06 +0200

    Added 'java' folder. Modifed various test files.

---
+++ java/Hi.java
@@ -0,0 +1,4 @@
+public class Hi {public static void main(String[] args) {
+		System.out.println("hi");
+	}
+}
---
+++ java/Intro.java
@@ -0,0 +1,15 @@
+public class Intro
+{
+	public static void main(String[] Args) {
+		int NumElements = Args.length;
+		for (int I = 1; I <= NumElements; I++) {
+			int Num;
+
+			Num = Integer.parseInt(Args[I-1]);
+			NumNode NN = new NumNode(Num);
+			NN.Insert();
+		}
+		System.out.println("Final Sorted List:");
+		NumNode.PrintList();
+	}
+}
